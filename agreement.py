68bb51ac78f4016b9d67aa9ea8a661295ea1f051
Marcin Wlodarczak <mwlodarczak@gmail.com>
2012-03-19 22:22:27 +0100

    Add functions creating contingency tables.

--- agreement.py
+++ agreement.py
@@ -19,6 +19,7 @@
 
 from __future__ import division
 
+import itertools
 import numpy as np
 
 # --------------
@@ -47,14 +48,15 @@ def fleiss_chance_agreement(a):
     return np.sum(np.square(per_category_probabilities(a)))
 
 def fleiss_kappa(a):
-    """Calculates Fleiss's kappa for the input array."""
+    """Calculates Fleiss's kappa for the input array (with categories
+    in columns and items in rows)."""
     p = observed_agreement(a)
     p_e = chance_agreement(a)
     return (p - p_e) / (1 - p_e)
 
-# --------------
+# -------------
 # Cohen's kappa
-# --------------
+# -------------
 
 def cohen_kappa(a):
     """Calculates Cohen's kappa for the input array."""
@@ -67,9 +69,9 @@ def cohen_kappa(a):
     p_e = np.sum((colsums * rowsums) / totsum**2)
     return (p - p_e) / (1 - p_e)
 
-# --------------
+# ----------
 # Scott's pi
-# --------------
+# ----------
 
 def scott_pi(a):
     """Calculates Scott's Pi for the input array."""
@@ -83,3 +85,33 @@ def scott_pi(a):
     p_e = np.sum(joint_marginal_props**2)
     return (p - p_e) / (1 - p_e)
 
+# ----------------------------------------------------------
+# Fuctions producing contingency tables from lists of labels
+# ----------------------------------------------------------
+
+
+def two_by_two_table(l1, l2):
+    """Returns a 2 by 2 contingency table given two lists of labels."""
+    if len(l1) != len(l2):
+        Exception('The numbers of labels differ.')
+    # List of unique labels from both lists.
+    categories = list(set(l1 + l2))
+    # Dictionary translating labels into consecutive integers.
+    trans = dict([(categories[x], x) for x in range(len(categories))])
+    cont_table = np.zeros([len(categories), len(categories)])
+    for labels in zip(l1, l2):
+        cont_table[trans[labels[0]], trans[labels[1]]] += 1
+    return cont_table
+
+def n_by_m_table(l):
+    """Returns a contingency table with categories in columns and
+    items in rows given a list of lists of labels."""
+    if len(l) < 2:
+        Exception('The number of lists to compare must be at least 2')
+    elif set([len(x) for x in l]) > 1:
+        Exception('The numbers of labels differ')
+    # List of unique labels from all lists.
+    categories = list(set(itertools.chain(*l)))
+    cont_table = np.array([x.count(y) for x in zip(*l) for y in categories])
+    cont_table.shape = (len(l[0]), len(categories))
+    return cont_table
