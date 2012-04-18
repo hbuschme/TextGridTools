60e9189b1a25c055857e5942e0f5f36e5a3cf5fa
Hendrik Buschmeier <hbuschme@TechFak.Uni-Bielefeld.DE>
2012-04-18 14:07:08 +0200

    Added license boilerplate.

--- scripts/print-tiernames.py
+++ scripts/print-tiernames.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
-'''Prints the name of the tiers of the specified textgrids. If a
-directory is specified, all tier names of teh .TextGrid files in
-this directory are printed.'''
+# -*- coding: utf-8 -*-
+
+# print-tiernames.py - Prints the tier names of a textrid file
+# Copyright (C) 2012 Hendrik Buschmeier
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Prints the name of the tiers of the specified textgrid files.
+# If a directory is specified, all tier names of the .TextGrid
+# files contained in this directory are printed.
+
+from __future__ import print_function, division
 
 import os
 import sys
