c475c494f2cddbef17ca3bbdea49ca362fe6b677
Hendrik Buschmeier <hbuschme@uni-bielefeld.de>
2016-11-07 23:39:23 +0100

    Bumped version number to 1.4.3.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from distutils.core import setup
 setup(
     name='tgt',
     description='TextGridTools -- Read, write, and manipulate Praat TextGrid files',
-    version='1.4.2',
+    version='1.4.3',
     packages=['tgt'],
     scripts=[
     	'scripts/tgt-concatenate-textgrids.py',
